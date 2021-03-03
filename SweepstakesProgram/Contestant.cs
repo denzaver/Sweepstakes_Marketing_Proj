28e99d5a3debc2b0fadfe1b25c24c72bd682e159
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-03 09:57:10 -0800

    Created a Contestant class which has a firstName, lastName, emailAddress, and registration number variable.

---
+++ SweepstakesProgram/Contestant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    class Contestant
+    {
+        //member variable (HAS A)
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+        //constructor
+
+        //member method (CAN DO)
+    }
+}
