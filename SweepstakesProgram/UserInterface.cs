b9f1fef6e44ee9bd9ad5bb4915e1758cb5daf81d
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-03 11:44:45 -0800

    Extablished a UnserInterface class, correct the mistake of creating it as an interface instead of a class, and created static methods to prompt the user to input for their first name, last name, email, and registration number and capture that information.

---
+++ SweepstakesProgram/UserInterface.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    static class UserInterface // needs to get info from the user
+    {
+
+        public static string GetFirstName()
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+            return firstName;
+        }
+
+        public static string GetLastName()
+        {
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+            return lastName;
+        }
+
+        public static string GetEmail()
+        {
+            Console.WriteLine("Please enter your email address:");
+            string emailAddress = Console.ReadLine();
+            return emailAddress;
+        }
+
+        public static int GetRegistrationNum()
+        {
+            Console.WriteLine("Please enter your email address:");
+            int registrationNumber = Convert.ToInt32(Console.ReadLine());
+            return registrationNumber;
+        }
+
+    }
+}
