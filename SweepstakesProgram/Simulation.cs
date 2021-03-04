41de5068692747986e28fc9549e59d680abcb7a9
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-04 14:10:37 -0800

    Created a Simulation class as well as a FactoryPatter class with the goal of collecting user input and allow them the choice to select which data structure they would like to use. Need to complete the logic and get it to work.

---
+++ SweepstakesProgram/Simulation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    class Simulation
+    {
+        public string GetChoice()
+        {
+            Console.WriteLine("Which data would you like to use?: Stack or Queue");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        
+    }
+}
