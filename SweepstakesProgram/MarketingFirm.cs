cc18b208ff3f9ec33b1ca6224a4338a8d241fce6
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-04 12:04:11 -0800

    Created the MarketinFirm class along with a private member variable of 'ISweepstakesManager' , constructor that passes in an ISweepstakesMnager' parameter and CreateSweepstakes method. Will need to complete the function logic for the CTOR and method.

---
+++ SweepstakesProgram/MarketingFirm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    class MarketingFirm
+    {
+        //member variable
+        private ISweepstakesManger _manager;
+
+        //property
+        
+        
+        //constructor
+        public MarketingFirm(ISweepstakesManger manager)
+        {
+
+        }
+
+        // member methods
+        public void CreateSweepstake()
+        {
+
+        }
+
+        
+
+    }
+}
