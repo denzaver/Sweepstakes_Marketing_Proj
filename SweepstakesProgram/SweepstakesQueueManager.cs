fcbf4e52ad485dd73f151fa15a9a08d0efd457e0
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-04 11:39:45 -0800

    Created the SweepstakesQueueManager class and methods to use the Queue data structre while.

---
+++ SweepstakesProgram/SweepstakesQueueManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    class SweepstakesQueueManager : ISweepstakesManger
+    {
+
+        Queue<Sweepstakes> queue = new Queue<Sweepstakes>();
+        public void InsertSweepstake(Sweepstakes sweepstakes)
+        {
+            queue.Enqueue(sweepstakes);
+        }
+
+        public Sweepstakes GetSweepStake()
+        {
+            return queue.Dequeue();
+        }
+    }
+}
