6c98448295bb93c94fbc4b2b12ed21afac5293a2
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-04 06:21:32 -0800

    Create the shells for the methods to be included within the Sweepstakes class, RegisterContestant, PickWinner, and PrintContestantInfo methods.

---
+++ SweepstakesProgram/Sweepstakes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProgram
+{
+    class Sweepstakes
+    {
+
+        // member variables
+        private Dictionary<int, Contestant> contestants;
+        private string name;
+
+        // property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        // constructor
+        public Sweepstakes(string name)
+        {
+            contestants = new Dictionary<int, Contestant>();
+        }
+
+        // member methods
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+            return null;
+        }
+
+        public void PrintContestantinfo(Contestant contestant)
+        {
+
+        }
+
+        
+    }
+}
